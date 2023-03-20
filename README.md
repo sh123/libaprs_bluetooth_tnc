32442907e2bfc7cd6432567d93a2173c004e69a1
sh123 <sh123@users.noreply.github.com>
2023-03-20 14:54:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# bluetooth_tnc
+Arduino bluetooth TNC based on customezed version of LibAPRS
+
+Arduino bluetooth TNC Modem
+===========================
+
+Introduction
+------------
+Arduino bluetooth TNC modem is based on customized version of LibAPRS, allows 
+simultaneously work using both bluetooth and USB-serial port. Uses next
+peripherals:
+
+ * Bluetooth serial adapter
+ * Micromodem from marqvist - http://unsigned.io/micromodem/
+
+Requirements
+------------
+
+ * Customized LibAPRS (aprs_tracker branch, see device.h for arduino ports usage) - https://github.com/sh123/LibAPRS/tree/aprs_tracker
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+
+#define OPEN_SQUELCH false
+#define ADC_REFERENCE REF_5V
+
+#include "LibAPRS.h"
+#include "KISS.h"
+
+extern Afsk modem;
+extern AX25Ctx AX25;
+
+char incomingByte;
+
+#ifdef TNC_CAR
+SoftwareSerial bluetooth(10, 11); // RX, TX (car)
+#pragma message("Building for /M TNC")
+#else
+SoftwareSerial bluetooth(12, 11); // RX, TX (tnc05, tnc06)
+#pragma message("Building for /P TNC")
+#endif
+
+void aprs_msg_callback(struct AX25Ctx *ctx) {
+  kiss_messageCallback(ctx);
+}
+
+void setup() {
+  Serial.begin(115200);
+  bluetooth.begin(9600);
+
+  APRS_init(ADC_REFERENCE, OPEN_SQUELCH);
+  //APRS_setTail(1000U);
+  kiss_init(&AX25, &modem, &Serial, &bluetooth);
+  //Serial.println("started");
+}
+
+void loop() {
+  APRS_poll();
+  while (bluetooth.available() > 0) {
+    incomingByte = bluetooth.read();
+    //Serial.write(incomingByte);
+    kiss_serialCallback(incomingByte);
+  }
+  while (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    kiss_serialCallback(incomingByte);
+  }
+}
